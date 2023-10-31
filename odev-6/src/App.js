0097b75acf417ed49352e75649b4bfce4358af9e
eda-kabay <edanur.kabay@gmail.com>
2023-10-31 20:06:13 +0300

    odev-6

---
+++ odev-6/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductGrid from './ProductGrid/ProductGrid'
+
+
+function App() {
+  
+  return (<>
+ <ProductGrid/>
+
+  </>
+  )
+}
+
+export default App
---
+++ odev-6/src/ProductGrid/ProductGrid.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { DataGrid } from '@mui/x-data-grid'
+import { Button } from "@mui/material";
+
+function ProductGrid() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios.get("https://northwind.vercel.app/api/products")
+      .then(res => {
+        setProducts(res.data)
+      })
+  }, [])
+
+  const handleDeleteClick = (id) => {
+    axios.delete("https://northwind.vercel.app/api/products/" + id)
+      .then(res => {
+        setProducts(products.filter((product) => product.id !== id));
+
+      })
+
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 200 },
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'unitPrice', headerName: 'Unit Price', type: 'number', width: 200 },
+    { field: 'unitsInStock', headerName: 'Units In Stock', type: 'number', width: 200 },
+    {
+      field: "delete",
+      headerName: "Delete",
+      width: 250,
+      renderCell: (params) => (
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => handleDeleteClick(params.row.id)}
+        >
+          Delete
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <DataGrid
+        columns={columns}
+        rows={products}
+        sx={{
+          "& .bg-red": {
+            backgroundColor: "red",
+          }}}
+        getRowClassName={(params) => {
+          if (params.row.unitsInStock === 0) {
+            return "bg-red";
+          }
+        }}
+      />
+    </div>
+  )
+}
+
+export default ProductGrid;
